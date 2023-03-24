1e74fa98da04bf7cd4906a92fe1f16390556f9c9
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-03-24 10:11:44 +0100

    Revert "responsive"
    This reverts commit cec152068d7f177a72c7fb20868dca35af0b5a93.

--- Positioning/scriptPositioning.js
+++ Positioning/scriptPositioning.js
@@ -74,18 +74,18 @@ export const positioning = () => {
 
       contentsElement += `
          <div class="settingsContents settingsContents--positioning">
-            <p class="settingsParagraph--positioning">.parent{</p>
+            <p class="settingsHeader">.parent{</p>
             <div>
                <p class="settingsParagraph settingsParagraph--positioning">  position: relative; </p>
                <p class="settingsParagraph settingsParagraph--positioning">  border: 3px dashed white; </p>
                <p class="settingsParagraph settingsParagraph--positioning">  background: purple; </p>
             </div>
-            <p class="settingsParagraph--positioning">}</p>
-            <p class="settingsParagraph--positioning">.child{</p>
+            <p class="settingsHeader">}</p>
+            <p class="settingsHeader">.child{</p>
             <div>
                ${settingsLabel()}
             </div>
-            <p class="settingsParagraph--positioning">}</p>
+            <p class="settingsHeader">}</p>
          </div>
          `;
 
