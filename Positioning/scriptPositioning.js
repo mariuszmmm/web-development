d193ce990d5c88cd6b7944d2cb854e7a04e472d7
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-03-25 11:25:20 +0100

    responsive

--- Positioning/scriptPositioning.js
+++ Positioning/scriptPositioning.js
@@ -64,7 +64,7 @@ export const positioning = () => {
                buttons.propertiesValues.forEach((prop) => {
                   if (prop.active) {
                      contentsElement += `:
-                     ${prop.active ? prop.name : ""} </p>`
+                     ${prop.active ? prop.name : ""}; </p>`
                   };
                });
             };
