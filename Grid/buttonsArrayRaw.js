32f93656127f0d24d10b3f52f46fc90c8fe9fe0a
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-04-04 22:14:43 +0200

    changed grid

--- Grid/buttonsArrayRaw.js
+++ Grid/buttonsArrayRaw.js
@@ -2,11 +2,11 @@ export const buttonsArrayRaw = [
   {
     properties: ["display"],
     propertiesValues: ["grid"],
-    destiny: ["parent"],
+    destiny: ["parent", "child_all"],
    },
   {
     properties: ["grid-template-columns"],
-    propertiesValues: ["auto", "auto auto", "1fr","1fr 2fr", "repeat(3, 1fr)", "repeat(2, 1fr 2fr)", "repeat(3, 100px)", "repeat(3, 100px 200px)", "repeat(3, 100px 1fr)", "100px", "25%", "100px 25% auto 1fr", "minmax(50px, 1fr)"],
+    propertiesValues: ["auto", "auto auto", "1fr","1fr 2fr", "repeat(3, 1fr)", "repeat(2, 1fr 2fr)", "repeat(3, 100px)", "repeat(3, 100px 200px)", "repeat(3, 100px 1fr)", "repeat(auto-fill, 200px)", "repeat(auto-fill, 1fr)", "repeat(auto-fill, minmax(200px, 1fr)", "100px", "25%", "100px 25% auto 1fr", "minmax(50px, 1fr)"],
     destiny: ["parent"],
    },
   {
@@ -26,12 +26,12 @@ export const buttonsArrayRaw = [
    },
   {
     properties: ["grid-auto-columns"],
-    propertiesValues: ["1fr 2fr", "50px 100px", "25% 50%"],
+    propertiesValues: ["100px", "250px", "50%", "50px 100px"],
     destiny: ["parent"],
   },
   {
     properties: ["grid-auto-rows"],
-    propertiesValues: ["1fr 2fr", "50px 100px", "25% 50%"],
+    propertiesValues: ["100px", "100px 150px", "25% 50%", "minmax(100px, auto)"],
     destiny: ["parent"],
    },
    {
@@ -62,26 +62,26 @@ export const buttonsArrayRaw = [
    {
      properties: ["align-content"],
      propertiesValues: ["stretch", "start", "end", "center", "space-between", "space-around", "space-evenly"],
-     destiny: ["parent"],
+     destiny: ["parent", "child_all"],
    },
   {
     properties: ["grid-column-start"],
-    propertiesValues: ["1", "2", "3", "-1", "-2", "-3"],
+    propertiesValues: ["1", "2", "3", "4", "-1", "-2", "-3"],
     destiny: ["child"],
    },
   {
-    properties: ["grid-row-start"],
-    propertiesValues: ["1", "2", "3", "-1", "-2", "-3"],
+    properties: ["grid-column-end"],
+    propertiesValues: ["1", "2", "3", "4", "-1", "-2", "-3", "span 2", "span 3"],
     destiny: ["child"],
   },
   {
-    properties: ["grid-column-end"],
-    propertiesValues: ["1", "2", "3", "-1", "-2", "-3", "span 2", "span 3"],
+    properties: ["grid-row-start"],
+    propertiesValues: ["1", "2", "3", "4", "-1", "-2", "-3"],
     destiny: ["child"],
-  },
+  },  
   {
     properties: ["grid-row-end"],
-    propertiesValues: ["1", "2", "3", "-1", "-2", "-3", "span 2", "span 3"],
+    propertiesValues: ["1", "2", "3", "4", "-1", "-2", "-3", "span 2", "span 3"],
     destiny: ["child"],
   },
  ];
