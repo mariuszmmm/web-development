c5f0fcb5461fb63a30c365fee43923753bbc5b29
mariuszmmm@op.pl <mariuszmmm@op.pl>
2023-04-05 21:58:57 +0200

    corected grid and flex

--- Grid/buttonsArrayRaw.js
+++ Grid/buttonsArrayRaw.js
@@ -6,32 +6,32 @@ export const buttonsArrayRaw = [
    },
   {
     properties: ["grid-template-columns"],
-    propertiesValues: ["auto", "auto auto", "1fr","1fr 2fr", "repeat(3, 1fr)", "repeat(2, 1fr 2fr)", "repeat(4, 100px)", "repeat(3, 100px 200px)", "repeat(3, 100px 1fr)", "repeat(auto-fill, 200px)", "repeat(auto-fill, 1fr)", "repeat(auto-fill, minmax(200px, 1fr)", "100px", "25%", "100px 25% auto 1fr", "minmax(50px, 1fr)"],
+    propertiesValues: ["none", "auto", "auto auto", "1fr","1fr 2fr", "repeat(3, 1fr)", "repeat(2, 1fr 2fr)", "repeat(4, 100px)", "repeat(3, 100px 200px)", "repeat(3, 100px 1fr)", "repeat(auto-fill, 200px)", "repeat(auto-fill, 1fr)", "repeat(auto-fill, minmax(200px, 1fr)", "100px", "25%", "100px 25% auto 1fr", "minmax(50px, 1fr)"],
     destiny: ["parent"],
    },
   {
     properties: ["grid-template-rows"],
-    propertiesValues: ["auto", "auto auto", "1fr","1fr 2fr", "repeat(3, 1fr)", "repeat(2, 1fr 2fr)", "repeat(4, 100px)", "repeat(3, 100px 200px)", "repeat(3, 100px 1fr)", "repeat(auto-fill, 200px)", "repeat(auto-fill, 1fr)", "repeat(auto-fill, minmax(200px, 1fr)", "100px", "25%", "100px 25% auto 1fr", "minmax(50px, 1fr)"],
+    propertiesValues: ["none", "auto", "auto auto", "1fr","1fr 2fr", "repeat(3, 1fr)", "repeat(2, 1fr 2fr)", "repeat(4, 100px)", "repeat(3, 100px 200px)", "repeat(3, 100px 1fr)", "repeat(auto-fill, 200px)", "repeat(auto-fill, 1fr)", "repeat(auto-fill, minmax(200px, 1fr)", "100px", "25%", "100px 25% auto 1fr", "minmax(50px, 1fr)"],
     destiny: ["parent"],
   },  
   {
     properties: ["grid-column-gap"],
-    propertiesValues: ["none", "10px", "10%", "10vw"],
+    propertiesValues: ["0", "10px", "10%", "10vw"],
     destiny: ["parent"],
    },
   {
     properties: ["grid-row-gap"],
-    propertiesValues: ["none", "10px", "10%", "10vw"],
+    propertiesValues: ["0", "10px", "10%", "10vw"],
     destiny: ["parent"],
    },
   {
     properties: ["grid-auto-columns"],
-    propertiesValues: ["100px", "250px", "50%", "50px 100px"],
+    propertiesValues: ["auto", "100px", "250px", "50%", "50px 100px"],
     destiny: ["parent"],
   },
   {
     properties: ["grid-auto-rows"],
-    propertiesValues: ["100px", "100px 150px", "25% 50%", "minmax(100px, auto)"],
+    propertiesValues: ["auto", "100px", "100px 150px", "25% 50%", "minmax(100px, auto)"],
     destiny: ["parent"],
    },
   {
@@ -53,6 +53,11 @@ export const buttonsArrayRaw = [
       properties: ["align-items"],
       propertiesValues: ["stretch", "start", "end", "center"],
        destiny: ["parent", "child_all"],
+   },
+  {
+    properties: ["grid-template-areas"],
+    propertiesValues: ["none", `"corner corner top top" "corner corner middle right" "corner corner middle right" "left bottom bottom bottom"`, `"corner top top right" "left middle middle right" "left middle middle right" "left bottom bottom right"`, `"top top top top" "left middle middle right" "left middle middle right" "corner bottom bottom right"`],
+    destiny: ["parent"],
    },
    {
     properties: ["justify-self"],
@@ -83,11 +88,6 @@ export const buttonsArrayRaw = [
     properties: ["grid-row-end"],
     propertiesValues: ["1", "2", "3", "-1", "-2", "-3", "span 2", "span 3"],
     destiny: ["child"],
-  },
-  {
-    properties: ["grid-template-areas"],
-    propertiesValues: [`"corner corner top top" "corner corner middle right" "corner corner middle right" "left bottom bottom bottom"`, `"corner top top right" "left middle middle right" "left middle middle right" "left bottom bottom right"`, `"top top top top" "left middle middle right" "left middle middle right" "corner bottom bottom right"`],
-    destiny: ["parent"],
   },
   {
     properties: ["grid-area"],
