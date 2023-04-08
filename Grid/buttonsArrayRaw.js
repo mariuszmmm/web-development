0d3e21c6e8f469562065b931dde74a5e1a042f72
mariuszmmm@op.pl <mariuszmmm@op.pl>
2023-04-08 14:53:53 +0200

    changed array grid

--- Grid/buttonsArrayRaw.js
+++ Grid/buttonsArrayRaw.js
@@ -13,17 +13,7 @@ export const buttonsArrayRaw = [
     properties: ["grid-template-rows"],
     propertiesValues: ["none", "auto", "auto auto", "1fr","1fr 2fr", "repeat(3, 1fr)", "repeat(2, 1fr 2fr)", "repeat(4, 100px)", "repeat(3, 100px 200px)", "repeat(3, 100px 1fr)", "repeat(auto-fill, 200px)", "repeat(auto-fill, 1fr)", "repeat(auto-fill, minmax(200px, 1fr)", "100px", "25%", "100px 25% auto 1fr", "minmax(50px, 1fr)"],
     destiny: ["parent"],
-  },  
-  {
-    properties: ["grid-column-gap"],
-    propertiesValues: ["0", "10px", "10%", "10vw"],
-    destiny: ["parent"],
-   },
-  {
-    properties: ["grid-row-gap"],
-    propertiesValues: ["0", "10px", "10%", "10vw"],
-    destiny: ["parent"],
-   },
+  },
   {
     properties: ["grid-auto-columns"],
     propertiesValues: ["auto", "100px", "250px", "50%", "50px 100px"],
@@ -32,6 +22,21 @@ export const buttonsArrayRaw = [
   {
     properties: ["grid-auto-rows"],
     propertiesValues: ["auto", "100px", "100px 150px", "25% 50%", "minmax(100px, auto)"],
+    destiny: ["parent"],
+  },
+  {
+    properties: ["grid-template-areas"],
+    propertiesValues: ["none", `"corner corner top top" "corner corner middle right" "corner corner middle right" "left bottom bottom bottom"`, `"corner top top right" "left middle middle right" "left middle middle right" "left bottom bottom right"`, `"top top top top" "left middle middle right" "left middle middle right" "corner bottom bottom right"`],
+    destiny: ["parent"],
+  },
+  {
+    properties: ["grid-column-gap"],
+    propertiesValues: ["0", "10px", "10%", "10vw"],
+    destiny: ["parent"],
+   },
+  {
+    properties: ["grid-row-gap"],
+    propertiesValues: ["0", "10px", "10%", "10vw"],
     destiny: ["parent"],
    },
   {
@@ -53,21 +58,6 @@ export const buttonsArrayRaw = [
       properties: ["align-items"],
       propertiesValues: ["stretch", "start", "end", "center"],
        destiny: ["parent", "child_all"],
-   },
-  {
-    properties: ["grid-template-areas"],
-    propertiesValues: ["none", `"corner corner top top" "corner corner middle right" "corner corner middle right" "left bottom bottom bottom"`, `"corner top top right" "left middle middle right" "left middle middle right" "left bottom bottom right"`, `"top top top top" "left middle middle right" "left middle middle right" "corner bottom bottom right"`],
-    destiny: ["parent"],
-   },
-   {
-    properties: ["justify-self"],
-    propertiesValues: ["stretch", "start", "end", "center"],
-    destiny: ["child"],
-   },   
-  {
-    properties: ["align-self"],
-    propertiesValues: ["stretch", "start", "end", "center"],
-    destiny: ["child"],
    },
   {
     properties: ["grid-column-start"],
@@ -94,4 +84,14 @@ export const buttonsArrayRaw = [
     propertiesValues: ["auto","corner", "top", "middle", "left", "right", "bottom"],
     destiny: ["child"],
   },
+   {
+     properties: ["justify-self"],
+     propertiesValues: ["stretch", "start", "end", "center"],
+     destiny: ["child"],
+   },
+   {
+     properties: ["align-self"],
+     propertiesValues: ["stretch", "start", "end", "center"],
+     destiny: ["child"],
+   },
  ];
